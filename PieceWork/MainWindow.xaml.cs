155812cd96f6fefd7cccc8a3618ead4e8c54642a
Arsalan Radhu <arsalan.radhu@dcmail.ca>
2021-09-25 15:36:42 -0400

    Completed all the requirements.

--- PieceWork/MainWindow.xaml.cs
+++ PieceWork/MainWindow.xaml.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Title: IncInc Payroll (Piecework)
+// Last Modified: 25 September 2021
+// Written By: Arsalan Arif Radhu
+// Description: Made a WPF form IncInc which accepts the worker name and the number of messages sent by them, and then calculates their pay as well as the total pay, total number of worker and the average pay.
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
+
+
 
 namespace PieceWork
 {
@@ -23,6 +18,58 @@ namespace PieceWork
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Validates and calculate pay per messages (using a class defined elsewhere) by instantiating the PieceWorker and dis[plays the various values for output using class properties. 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CalculateClick(object sender, RoutedEventArgs e)
+        {
+            //Attempt to make a new worker
+            PieceworkWorker worker = new PieceworkWorker(textBoxWorkerName.Text, textBoxMessagesSent.Text);
+
+            // Display worker's pay
+            textBoxSinglePay.Text = worker.Pay.ToString();
+            //Display he summary values
+            textBoxTotalPay.Text = worker.TotalPay.ToString();
+            textBoxAveragePay.Text = worker.AveragePay.ToString();
+            textBoxTotalWorkers.Text = worker.TotalWorkers.ToString(); 
+
+            // Disable input related fields
+            textBoxWorkerName.IsReadOnly = true;
+            textBoxMessagesSent.IsReadOnly = true;
+            buttonCalculate.IsEnabled = false;
+
+            //If the worker is not created successfully, show error.
+        }
+
+        /// <summary>
+        /// Reset the form to its default state with all the text input and output fields cleared.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ClearClick(object sender, RoutedEventArgs e)
+        {
+            // Clear all input fields
+            textBoxWorkerName.Clear();
+            textBoxMessagesSent.Clear();
+            
+            //Clear all output fields
+            textBoxSinglePay.Clear();
+            textBoxTotalWorkers.Clear();
+            textBoxTotalPay.Clear();
+            textBoxAveragePay.Clear();
+
+            //Renable any controls that may be disabled
+            textBoxWorkerName.IsReadOnly = false;
+            textBoxMessagesSent.IsReadOnly = false;
+            buttonCalculate.IsEnabled = true;
+
+            //Set Focus
+            textBoxWorkerName.Focus();
+
         }
     }
 }
--- PieceWork/PieceworkWorker.cs
+++ PieceWork/PieceworkWorker.cs
@@ -1,200 +1,282 @@
-﻿//// PieceworkWorker.cs
-////         Title: IncInc Payroll (Piecework)
-//// Last Modified: 
-////    Written By: 
-//// Adapted from PieceworkWorker by Kyle Chapman, September 2019
-//// 
-//// This is a class representing individual worker objects. Each stores
-//// their own name and number of messages and the class methods allow for
-//// calculation of the worker's pay and for updating of shared summary
-//// values. Name and messages are received as strings.
-//// This is being used as part of a piecework payroll application.
+﻿// PieceworkWorker.cs
+//         Title: IncInc Payroll (Piecework)
+// Last Modified: 25 September 2021
+//    Written By: Arsalan Arif Radhu
+// Adapted from PieceworkWorker by Kyle Chapman, September 2019
+// 
+// This is a class representing individual worker objects. Each stores
+// their own name and number of messages and the class methods allow for
+// calculation of the worker's pay and for updating of shared summary
+// values. Name and messages are received as strings.
+// This is being used as part of a piecework payroll application.
 
-//// This is currently incomplete; note the four comment blocks
-//// below that begin with "TO DO"
+// This is currently incomplete; note the four comment blocks
+// below that begin with "TO DO"
 
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 
-//namespace PieceWork // Ensure this namespace matches your own
-//{
-//    class PieceworkWorker
-//    {
+namespace PieceWork // Ensure this namespace matches your own
+{
+    class PieceworkWorker
+    {
 
-//        #region "Variable declarations"
+        #region "Variable declarations"
 
-//        // Instance variables
-//        private string employeeName;
-//        private int employeeMessages;
-//        private decimal employeePay;
+        // Instance variables
+        private string employeeName;
+        private int employeeMessages;
+        private decimal employeePay;
 
-//        private bool isValid = true;
+        private bool isValid = true;
 
-//        // Shared class variables
-//        private static int overallNumberOfEmployees;
-//        private static int overallMessages;
-//        private static decimal overallPayroll;
+        // Shared class variables
+        private static int overallNumberOfEmployees;
+        private static int overallMessages;
+        private static decimal overallPayroll;
 
-//        #endregion
+        //CONSTANTS
+        private const int ZERO = 0;
+        private const int firstThreshold = 1250;
+        private const int secondThreshold = 2500;
+        private const int thirdThreshold = 3750;
+        private const int lastThreshold = 5000;
 
-//        #region "Constructors"
+        private const decimal firstThresholdPay = 0.02M;
+        private const decimal secondThresholdPay = 0.024M;
+        private const decimal thirdThresholdPay = 0.028M;
+        private const decimal fourthThresholdPay = 0.034M;
+        private const decimal lastThresholdPay = 0.04M;
 
-//        /// <summary>
-//        /// PieceworkWorker constructor: accepts a worker's name and number of
-//        /// messages, sets and calculates values as appropriate.
-//        /// </summary>
-//        /// <param name="nameValue">the worker's name</param>
-//        /// <param name="messageValue">a worker's number of messages sent</param>
-//        public PieceworkWorker(string nameValue, string messagesValue)
-//        {
-//            // Validate and set the worker's name
-//            this.Name = nameValue;
-//            // Validate Validate and set the worker's number of messages
-//            this.Messages = messagesValue;
-//            // Calculcate the worker's pay and update all summary values
-//            findPay();
-//        }
+        #endregion
 
-//        /// <summary>
-//        /// PieceworkWorker constructor: empty constructor used strictly for inheritance and instantiation
-//        /// </summary>
-//        public PieceworkWorker()
-//        {
+        #region "Constructors"
 
-//        }
+        /// <summary>
+        /// PieceworkWorker constructor: empty constructor used strictly for inheritance and instantiation
+        /// </summary>
+        public PieceworkWorker()
+        {
 
-//        #endregion
+        }
 
-//        #region "Class methods"
+        /// <summary>
+        /// PieceworkWorker constructor: accepts a worker's name and number of
+        /// messages, sets and calculates values as appropriate.
+        /// </summary>
+        /// <param name="nameValue">the worker's name</param>
+        /// <param name="messageValue">a worker's number of messages sent</param>
+        public PieceworkWorker(string nameValue, string messagesValue)
+        {
+            // Validate and set the worker's name
+            this.Name = nameValue;
+            // Validate Validate and set the worker's number of messages
+            this.Messages = messagesValue;
+            // Calculcate the worker's pay and update all summary values
+            FindPay();
+        }
 
-//        /// <summary>
-//        /// Currently called in the constructor, the findPay() method is
-//        /// used to calculate a worker's pay using threshold values to
-//        /// change how much a worker is paid per message. This also updates
-//        /// all summary values.
-//        /// </summary>
-//        private void findPay()
-//        {
-//            // TO DO
-//            // Fill in this entire method by following the instructions provided
-//            // in the NETD 3202 Lab 1 handout
-//            // It is suggested that you use the requirements as a checklist in
-//            // order to ensure you don't miss any requirements.
+        
 
-//        }
+        #endregion
 
-//        #endregion
+        #region "Class methods"
 
-//        #region "Property Procedures"
+        /// <summary>
+        /// Currently called in the constructor, the findPay() method is
+        /// used to calculate a worker's pay using threshold values to
+        /// change how much a worker is paid per message. This also updates
+        /// all summary values.
+        /// </summary>
+        private void FindPay()
+        {
+            // TO DO
+            // Fill in this entire method by following the instructions provided
+            // in the NETD 3202 Lab 1 handout
+            // It is suggested that you use the requirements as a checklist in
+            // order to ensure you don't miss any requirements.
 
-//        /// <summary>
-//        /// Gets and sets a worker's name
-//        /// </summary>
-//        /// <returns>an employee's name</returns>
-//        public string Name
-//        {
-//            get
-//            {
-//                return employeeName;
-//            }
-//            set
-//            {
-//                // TO DO
-//                // Add validation for the worker's name based on the requirements
-//                // document
+            // Check if worker is considered valid.
+            if (isValid)
+            {
+                // If the worker is valid, check which range they are in 
+                if (employeeMessages < firstThreshold && employeeMessages > ZERO)
+                {
+                    employeePay = (decimal) (employeeMessages * firstThresholdPay);
+                }
+                else if (employeeMessages < secondThreshold && employeeMessages >= firstThreshold)
+                {
+                    employeePay = (decimal)(employeeMessages * secondThresholdPay);
+                }
+                else if (employeeMessages < thirdThreshold && employeeMessages >= secondThreshold)
+                {
+                    employeePay = (decimal)(employeeMessages * thirdThresholdPay);
+                }
+                else if (employeeMessages < lastThreshold && employeeMessages >= thirdThreshold)
+                {
+                    employeePay = (decimal)(employeeMessages * fourthThresholdPay);
+                }
+                else if(employeeMessages >= lastThreshold)
+                {
+                    employeePay = (decimal)(employeeMessages * lastThresholdPay);
+                }
 
-//                employeeName = value;
-//            }
-//        }
+                // Update all summary variables
+                overallNumberOfEmployees++;
+                overallMessages += employeeMessages;
+                overallPayroll += employeePay;
+            }
+            else
+            {
+                MessageBox.Show("INVALID ENTRY!!");
+            }
+        }
 
-//        /// <summary>
-//        /// Gets and sets the number of messages sent by a worker
-//        /// </summary>
-//        /// <returns>an employee's number of messages</returns>
-//        public string Messages
-//        {
-//            get
-//            {
-//                return employeeMessages.ToString();
-//            }
-//            set
-//            {
-//                // TO DO
-//                // Add validation for the number of messages based on the
-//                // requirements document
+        #endregion
 
-//                employeeMessages = Convert.ToInteger(value); // This line is dangerous and should probably never appear in your code. Can you explain why? Post about it on the Q&A board and I'll give you a stock.
+        #region "Property Procedures"
+
+        /// <summary>
+        /// Gets and sets a worker's name
+        /// </summary>
+        /// <returns>an employee's name</returns>
+        public string Name
+        {
+            get
+            {
+                return employeeName.ToString();
+            }
+            set
+            {
+                // TO DO
+                // Add validation for the worker's name based on the requirements
+                // document
+                if (string.IsNullOrEmpty(value))
+                {
+                    MessageBox.Show("Please Enter a name!", "Invalid Input ");
+                }
+                else
+                {
+                    employeeName = value;
+                }
+               
+            }
+        }
+
+        /// <summary>
+        /// Gets and sets the number of messages sent by a worker
+        /// </summary>
+        /// <returns>an employee's number of messages</returns>
+        public string Messages
+        {
+            get
+            {
+                return employeeMessages.ToString();
+            }
+            set
+            {
+                // TO DO
+                // Add validation for the number of messages based on the
+                // requirements document
+
+                // Check if number of messages entered in whole number or not
+                if (int.TryParse(value, out employeeMessages))
+                {
+                    // If number entered is whole number, check if it is in a valid range
+                    if (employeeMessages < ZERO)
+                    {
+                        // If not in valid range, report error
+                        isValid = false;
+                        MessageBox.Show("The number of messages should be greater than ZERO!","Invalid Entry!");
+                    }
+                }
+                else
+                {
+                    //If not a numeric value, report an error
+                    isValid = false;
+                    MessageBox.Show("The number of message entered should be a WHOLE number!" , "Invalid Entry!");
+                }
 
 
-//            }
-//        }
+            }
+        }
 
-//        /// <summary>
-//        /// Gets the worker's pay
-//        /// </summary>
-//        /// <returns>a worker's pay</returns>
-//        public decimal Pay
-//        {
-//            get
-//            {
-//                return employeePay;
-//            }
-//        }
+        /// <summary>
+        /// Gets the worker's pay
+        /// </summary>
+        /// <returns>a worker's pay</returns>
+        public decimal Pay
+        {
+            get
+            {
+                return employeePay;
+            }
+        }
 
-//        /// <summary>
-//        /// Gets the overall total pay among all workers
-//        /// </summary>
-//        /// <returns>the overall total pay among all workers</returns>
-//        public static decimal TotalPay
-//        {
-//            get
-//            {
-//                return overallPayroll;
-//            }
-//        }
+        /// <summary>
+        /// Gets the overall total pay among all workers
+        /// </summary>
+        /// <returns>the overall total pay among all workers</returns>
+        public decimal TotalPay
+        {
+            get
+            {
+                return overallPayroll;
+            }
+        }
 
-//        /// <summary>
-//        /// Gets the overall number of workers
-//        /// </summary>
-//        /// <returns>the overall number of workers</returns>
-//        public static int TotalWorkers
-//        {
-//            get
-//            {
-//                return overallNumberOfEmployees;
-//            }
-//        }
+        /// <summary>
+        /// Gets the overall number of workers
+        /// </summary>
+        /// <returns>the overall number of workers</returns>
+        public int TotalWorkers
+        {
+            get
+            {
+                return overallNumberOfEmployees;
+            }
+        }
 
-//        /// <summary>
-//        /// Gets the overall number of messages sent
-//        /// </summary>
-//        /// <returns>the overall number of messages sent</returns>
-//        public static int TotalMessages()
-//        {
-//            get
-//            {
-//                return overallMessages;
-//            }
-//        }
+        /// <summary>
+        /// Gets the overall number of messages sent
+        /// </summary>
+        /// <returns>the overall number of messages sent</returns>
+        public int TotalMessages
+        {
+            get
+            {
+                return overallMessages;
+            }
+        }
 
-//        /// <summary>
-//        /// Calculates and returns an average pay among all workers
-//        /// </summary>
-//        /// <returns>the average pay among all workers</returns>
-//        public static decimal AveragePay
-//        {
-//            get
-//            {
-//                // TO DO
-//                // Write the logic that will return the average pay among all workers. Test this carefully!
-//                return;
-//            }
-//        }
+        /// <summary>
+        /// Calculates and returns an average pay among all workers
+        /// </summary>
+        /// <returns>the average pay among all workers</returns>
+        public  decimal AveragePay
+        {
+            get
+            {
+                // TO DO
+                // Write the logic that will return the average pay among all workers. Test this carefully!
+                if (TotalWorkers == 0 || TotalPay == 0)
+                {
+                    return ZERO;
+                }
+                else
+                {
+                    return TotalPay / TotalWorkers;
+                }
+                
+            }
+        }
 
-//        #endregion
+        #endregion
 
-//    }
-//}
+    }
+}
